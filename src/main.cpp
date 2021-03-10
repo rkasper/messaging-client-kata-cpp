3d69aaead0c970466870976cc11f9a72a59cc085
Richard Kasperowski <r@kasperowski.com>
2021-03-10 16:47:45 -0500

    Completed "1) Send a message to a server"

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    string to = "World!";
-    if (argc > 1)
-    {
-        to = argv[1];
-    }
+int main(__unused int argc, char *argv[]) {
+    string to = argv[1];
+    string message = argv[2];
 
-    cout << "Hello, " << to << endl;
+    // File manipulation code borrowed from https://www.guru99.com/cpp-file-read-write-open.html
+    fstream fake_network;
+    fake_network.open("/tmp/fake_network.txt", ios::out);
+    if (!fake_network) {
+        cout << "File not created!";
+    }
+    else {
+        fake_network << "connect smtp" << endl;
+        fake_network << "To: " << to << endl;
+        fake_network << endl;
+        fake_network << message << endl;
+        fake_network << endl;
+        fake_network << "disconnect" << endl;
+
+        fake_network.close();
+    }
 
     return 0;
 }
