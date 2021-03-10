3d7cefcf555c47ca1ab184577d1ccdf33aab12ca
Richard Kasperowski <r@kasperowski.com>
2021-03-10 16:54:33 -0500

    "no @ sign" in progress

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,27 @@ int main(__unused int argc, char *argv[]) {
     string to = argv[1];
     string message = argv[2];
 
-    // File manipulation code borrowed from https://www.guru99.com/cpp-file-read-write-open.html
-    fstream fake_network;
-    fake_network.open("/tmp/fake_network.txt", ios::out);
-    if (!fake_network) {
-        cout << "File not created!";
+    if (to.find('@') == string::npos) {
+        cout << "Invalid email address: no at sign" << endl;
     }
-    else {
-        fake_network << "connect smtp" << endl;
-        fake_network << "To: " << to << endl;
-        fake_network << endl;
-        fake_network << message << endl;
-        fake_network << endl;
-        fake_network << "disconnect" << endl;
+    else
+    {
+        // File manipulation code borrowed from https://www.guru99.com/cpp-file-read-write-open.html
+        fstream fake_network;
+        fake_network.open("/tmp/fake_network.txt", ios::out);
+        if (!fake_network) {
+            cout << "File not created!";
+        }
+        else {
+            fake_network << "connect smtp" << endl;
+            fake_network << "To: " << to << endl;
+            fake_network << endl;
+            fake_network << message << endl;
+            fake_network << endl;
+            fake_network << "disconnect" << endl;
 
-        fake_network.close();
+            fake_network.close();
+        }
     }
 
     return 0;
