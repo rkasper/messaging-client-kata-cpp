5add79a304fe05f48d7b2e065b24c1689d019342
Richard Kasperowski <r@kasperowski.com>
2021-03-10 16:57:50 -0500

    Completed "2) Validate email address"

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,36 @@
 
 using namespace std;
 
+void send_message(const string &to, const string &message);
+
 int main(__unused int argc, char *argv[]) {
     string to = argv[1];
     string message = argv[2];
 
     if (to.find('@') == string::npos) {
         cout << "Invalid email address: no at sign" << endl;
-    }
-    else
-    {
-        // File manipulation code borrowed from https://www.guru99.com/cpp-file-read-write-open.html
-        fstream fake_network;
-        fake_network.open("/tmp/fake_network.txt", ios::out);
-        if (!fake_network) {
-            cout << "File not created!";
-        }
-        else {
-            fake_network << "connect smtp" << endl;
-            fake_network << "To: " << to << endl;
-            fake_network << endl;
-            fake_network << message << endl;
-            fake_network << endl;
-            fake_network << "disconnect" << endl;
-
-            fake_network.close();
-        }
+    } else {
+        send_message(to, message);
     }
 
     return 0;
+}
+
+void send_message(const string &to,
+                  const string &message) {
+    // File manipulation code borrowed from https://www.guru99.com/cpp-file-read-write-open.html
+    fstream fake_network;
+    fake_network.open("/tmp/fake_network.txt", ios::out);
+    if (!fake_network) {
+        cout << "File not created!";
+    } else {
+        fake_network << "connect smtp" << endl;
+        fake_network << "To: " << to << endl;
+        fake_network << endl;
+        fake_network << message << endl;
+        fake_network << endl;
+        fake_network << "disconnect" << endl;
+
+        fake_network.close();
+    }
 }
