81091679c74d7f022a2bbe30bbe787b5721e7d22
Richard Kasperowski <r@kasperowski.com>
2021-03-11 18:19:20 -0500

    It works. Refactoring is in progress.

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,66 @@
 
 using namespace std;
 
-void send_message(const vector<string> &recipients, const string &message);
+void send_email(const vector<string> &recipients, const string &message);
+void send_im(const string &recipient, const string &message);
 vector<string> split(string str, string token);
+void validate_email_address_or_exit(const string &elem);
 
 int main(int argc, char *argv[]) {
-    // TODO Make sure both command-line args are present
-    vector<string> recipients = split(argv[1], ",");
+    // TODO Make sure command-line args are present before you go asking for them.
+    if ((string)argv[1] == "-im") { // It's an IM.
+        cout << "It's an IM." << endl;
+        send_im(argv[2], argv[3]);
+    } else { // It's an email.
+        cout << "It's an email." << endl;
 
-    string message;
-    if (argc > 2 && ((string)argv[2]).length() > 0) {
-        message = argv[2];
-    } else {
-        // TODO Send error messages recipients cerr?
-        cout << "Cannot send an email with no body." << endl;
-        return 1;
-    }
+        vector<string> recipients = split(argv[1], ",");
 
-    for(auto & elem : recipients) {
-        if (elem.find('@') == string::npos) {
-            cout << elem << ": Invalid email address: no at sign" << endl;
-            return 1;
+        string message;
+        if (argc > 2 && ((string)argv[2]).length() > 0) {
+            message = argv[2];
+        } else {
+            // TODO Send error messages recipients cerr?
+            cout << "Cannot send an email with no body." << endl;
+            exit(1);
         }
-    }
 
-    send_message(recipients, message);
+        for(auto & elem : recipients) {
+            validate_email_address_or_exit(elem);
+        }
+
+        send_email(recipients, message);
+    }
 
     return 0;
 }
 
-void send_message(const vector<string> &recipients,
-                  const string &message) {
+void send_im(const string &recipient,
+             const string &message) {// File manipulation code borrowed from https://www.guru99.com/cpp-file-read-write-open.html
+    validate_email_address_or_exit(recipient);
+
+    fstream fake_network;
+    fake_network.open("/tmp/fake_network.txt", ios::out);
+    if (!fake_network) {
+        cout << "File not created!";
+    } else {
+        fake_network << "connect chat" << endl;
+        fake_network << "<" << recipient << ">:-> " << message << endl;
+        fake_network << "disconnect" << endl;
+
+        fake_network.close();
+    }
+}
+
+void validate_email_address_or_exit(const string &elem) {
+    if (elem.find('@') == string::npos) {
+        cout << elem << ": Invalid email address: no at sign" << endl;
+        exit(1);
+    }
+}
+
+void send_email(const vector<string> &recipients,
+                const string &message) {
     // File manipulation code borrowed from https://www.guru99.com/cpp-file-read-write-open.html
     fstream fake_network;
     fake_network.open("/tmp/fake_network.txt", ios::out);
